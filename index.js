bad67b10809487bfd7f8794820f7485611804ff0
Ben Drucker <bvdrucker@gmail.com>
2015-07-07 08:33:12 -0400

    Use get-package to handle local/npm json fetching

--- index.js
+++ index.js
@@ -2,10 +2,8 @@
 
 var extend = require('xtend')
 var ap = require('ap')
-var packageJson = require('package-json')
+var getPackage = require('get-package')
 var parseDependencies = require('semver-range-types')
-var mothership = require('mothership')
-var path = require('path')
 
 var applyDefaults = ap.partial(extend, {
   version: '',
@@ -21,28 +19,9 @@ module.exports = function semverOperators (pkgName, options, callback) {
 
   options = applyDefaults(options)
 
-  // if our path does not start w/ a dot, assume it's a package name ask npm
-  // for the json
-  if (pkgName.charAt(0) !== '.') {
-    return packageJson(pkgName, options.version || 'latest', parse)
-  }
-
-  // otherwise find a local package.json
-  mothership(path.resolve(options.cwd, pkgName), Boolean, function (err, pkg) {
-    if (!err && !pkg) err = new Error('package.json not found')
-    if (err) return callback(err)
-    var json = pkg.pack
-    // if we requested a specific version and the local version doesn't match,
-    // start over and ask npm
-    if (options.version && options.version !== json.version) {
-      return semverOperators(json.name, options, callback)
-    }
-    parse(err, json)
-  })
-
-  function parse (err, json) {
+  getPackage(pkgName, options, function (err, json) {
     if (err) return callback(err)
     var dependencies = json[options.type] || {}
     callback(null, parseDependencies(dependencies))
-  }
+  })
 }
